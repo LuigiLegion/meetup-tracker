4568dc6f7fdab67b090741de845ea498dab501be
Tal Luigi <luigi.t.agr@gmail.com>
2021-01-11 00:23:23 -0500

    chore: removed the root reducer logic from the store index file and replaced it with a root reducer import statement

--- client/store/index.js
+++ client/store/index.js
@@ -1,23 +1,16 @@
 // Imports
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-import userReducer from './reducers/userReducer'
-import layoutReducer from './reducers/layoutReducer'
-import meetupsReducer from './reducers/meetupsReducer'
+import rootReducer from './rootReducer'
 
 // Initializations
-const reducer = combineReducers({
-  user: userReducer,
-  meetups: meetupsReducer,
-  layout: layoutReducer
-})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(rootReducer, middleware)
 
 // Exports
 export default store
