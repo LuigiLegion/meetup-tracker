fc49f729a0bc74db93509d63d58bf4e66a2a3e70
Tal Luigi <luigi.t.agr@gmail.com>
2021-01-16 01:03:59 -0500

    chore: updated the Group component export statement in the components index file

--- client/components/index.js
+++ client/components/index.js
@@ -1,16 +1,15 @@
+// Exports
 /**
  * `components/index.js` exists simply as a 'central export' for our components.
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  **/
-
-// Exports
 export {default as PageNotFound} from './404/PageNotFound'
 export {default as Home} from './dashboard/Home'
 export {default as Links} from './links/Links'
 export {default as LinksBurger} from './links/LinksBurger'
 export {default as Groups} from './meetups/Groups'
-export {default as SingleGroup} from './meetups/SingleGroup'
+export {default as Group} from './meetups/Group'
 export {default as Hello} from './navbar/Hello'
 export {default as Navbar} from './navbar/Navbar'
 export {default as Preloader} from './navbar/Preloader'
