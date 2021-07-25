84742e154abaaef62c6aa264df2e7cac77bfafdd
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-25 12:49:19 -0400

    feat: refactored the Home component to use the MadeBy component

--- client/components/dashboard/Home.js
+++ client/components/dashboard/Home.js
@@ -1,7 +1,7 @@
 // Imports
 import React, {Fragment} from 'react'
 
-import {Groups} from '..'
+import {Groups, MadeBy} from '..'
 
 // Component
 const Home = () => {
@@ -9,18 +9,7 @@ const Home = () => {
     <Fragment>
       <Groups />
 
-      <div className="center">
-        {'Made with ❤ by '}
-
-        <a
-          className="author-link"
-          href="https://www.linkedin.com/in/tal-luigi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="text-style-bold text-color-blue">Tal Luigi</span>
-        </a>
-      </div>
+      <MadeBy />
     </Fragment>
   )
 }
