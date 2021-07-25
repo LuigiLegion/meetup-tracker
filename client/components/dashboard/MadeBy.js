7928fb86b36f5a8c78e0aea9c53fe2b198e51470
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-25 12:48:23 -0400

    feat: moved the made by logic from the Home component into the standalone MadeBy component

---
+++ client/components/dashboard/MadeBy.js
@@ -0,0 +1,23 @@
+// Imports
+import React from 'react'
+
+// Component
+const MadeBy = () => {
+  return (
+    <div className="center">
+      {'Made with ❤ by '}
+
+      <a
+        className="author-link"
+        href="https://www.linkedin.com/in/tal-luigi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="text-style-bold text-color-blue">Tal Luigi</span>
+      </a>
+    </div>
+  )
+}
+
+// Exports
+export default MadeBy
